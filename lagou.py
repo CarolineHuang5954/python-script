7b71508407748fe9d74385cae782b114d690fdb8
huangxinmiao <huangxm5954@icloud.com>
2020-03-05 14:53:55 +0800

    Add 拉钩网爬虫脚本 and 豆瓣电影Top250爬虫脚本

---
+++ lagou.py
@@ -0,0 +1,104 @@
+import random
+import time
+
+import requests
+from openpyxl import Workbook
+import pymysql.cursors
+
+
+def get_conn():
+    '''建立数据库连接'''
+    conn = pymysql.connect(host='localhost',
+                           user='root',
+                           password='root',
+                           db='python',
+                           charset='utf8mb4',
+                           cursorclass=pymysql.cursors.DictCursor)
+    return conn
+
+
+def insert(conn, info):
+    '''数据写入数据库'''
+    with conn.cursor() as cursor:
+        sql = "INSERT INTO `python` (`shortname`, `fullname`, `industryfield`, `companySize`, `salary`, `city`, `education`) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+        cursor.execute(sql, info)
+    conn.commit()
+
+
+def get_json(page, lang_name):
+    # 主url
+    url1 = 'https://www.lagou.com/jobs/list_python?city=%E5%85%A8%E5%9B%BD&cl=false&fromSearch=true&labelWords=&suginput='
+    # ajax请求
+    url = "https://www.lagou.com/jobs/positionAjax.json?px=default&needAddtionalResult=false"
+    # url = 'https://www.lagou.com/jobs/positionAjax.json?city={}&needAddtionalResult=false'.format(i)
+    '''返回当前页面的信息列表'''
+    # headers = {
+    #     'Host': 'www.lagou.com',
+    #     'Connection': 'keep-alive',
+    #     'Content-Length': '23',
+    #     'Origin': 'https://www.lagou.com',
+    #     'X-Anit-Forge-Code': '0',
+    #     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:72.0) Gecko/20100101 Firefox/72.0',
+    #     'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+    #     'Accept': 'application/json, text/javascript, */*; q=0.01',
+    #     'X-Requested-With': 'XMLHttpRequest',
+    #     'X-Anit-Forge-Token': 'None',
+    #     'Referer': 'https://www.lagou.com/jobs/list_%E8%BF%90%E7%BB%B4?city=%E6%88%90%E9%83%BD&cl=false&fromSearch=true&labelWords=&suginput=',
+    #     'Accept-Encoding': 'gzip, deflate, br',
+    #     'Accept-Language': 'en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7'
+    # }
+    #下面这个headers可以
+    headers = {
+        'Accept': 'application/json, text/javascript, */*; q=0.01',
+        'Referer': 'https://www.lagou.com/jobs/list_%E8%BF%90%E7%BB%B4?city=%E6%88%90%E9%83%BD&cl=false&fromSearch=true&labelWords=&suginput=',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36'
+    }
+    s = requests.Session()  # 建立session
+    s.get(url=url1, headers=headers, timeout=3)
+    cookie = s.cookies  # 获取cookie
+    print(cookie)
+    data = {'first': 'false', 'pn': page, 'kd': lang_name}
+    json = s.post(url=url, data=data, headers=headers, cookies=cookie,timeout=3).json()
+    time.sleep(7)
+    print(json)
+    list_con = json['content']['positionResult']['result']
+    info_list = []
+    for i in list_con:
+        info = []
+        info.append(i.get('companyShortName', '无'))
+        info.append(i.get('companyFullName', '无'))
+        info.append(i.get('industryField', '无'))
+        info.append(i.get('companySize', '无'))
+        info.append(i.get('salary', '无'))
+        info.append(i.get('city', '无'))
+        info.append(i.get('education', '无'))
+        info_list.append(info)
+    return info_list
+
+
+def main():
+    lang_name = 'python'
+    wb = Workbook()  # 打开 excel 工作簿
+    conn = get_conn()  # 建立数据库连接  不存数据库 注释此行
+    for i in ['北京', '上海', '广州', '深圳', '杭州']:  # 五个城市
+        page = 1
+        ws1 = wb.active
+        ws1.title = lang_name
+
+        while page < 31:  # 每个城市30页信息
+            info = get_json(page, lang_name)
+            if info is None:
+                return
+            page += 1
+            print(i, 'page', page)
+            time.sleep(random.randint(10, 20))
+            print(info)
+            for row in info:
+                insert(conn, tuple(row))  # 插入数据库，若不想存入 注释此行
+                ws1.append(row)
+    conn.close()  # 关闭数据库连接，不存数据库 注释此行
+    wb.save('{}职位信息.xlsx'.format(lang_name))
+
+
+if __name__ == '__main__':
+    main()
---
+++ webcrawlerdemo.py
@@ -0,0 +1,142 @@
+# coding=utf-8
+
+# 学习回顾：
+#   1. 涉及到引用的工具包
+#       a. 访问url使用requests库, 方法 requests.get()，需要加header否则会被反爬监测到返回418, response.text获取html
+#       b. 使用正则表达式用re库, re.compile()编译, re.findall()匹配, re.sub()
+#       c. 解析html使用BeautifulSoup，html.parser, soup.find_all()的使用
+#       d. excel工具包使用xlwt, xlwt.Workbook() -> book.add_sheet() -> sheet.write() -> book.save():创建文档->创建sheet->写入数据->保存
+#   2. 基础语法
+#       a. Sting处理, 字符串截取方式str[a:b]，转换字符串str(), split()
+#       b. 正则表达式的使用，占位符，匹配类型等
+#       c. 循环语句，条件语句
+#       d. 数组：拼接 list.append(), 插入元素 list.insert()
+#   3. 访问url获取html
+
+import sys
+from bs4 import BeautifulSoup
+import re
+import requests
+import xlwt
+
+# 得到页面全部内容
+def askURL(url):
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:72.0) Gecko/20100101 Firefox/72.0'}
+    resp = requests.get(url, headers=headers)  # 必须是headers=headers 否则418
+    html = resp.text
+    # print(html)
+    # print(resp.status_code)
+    return html
+
+# 获取相关内容
+def getData(baseurl):
+    findLink = re.compile(r'<a href="(.*?)">')  # 找到影片详情链接
+    findImgSrc = re.compile(r'<img.*src="(.*?)"', re.S)  # 找到影片图片
+    findTitle = re.compile(r'<span class="title">(.*)</span>')  # 找到片名
+    findRating = re.compile(r'<span class="rating_num" property="v:average">(.*)</span>')  # 找到评分
+    findJudge = re.compile(r'<span>(\d*)人评价</span>')  # 找到评价人数
+    findInq = re.compile(r'<span class="inq">(.*)</span>')  # 找到概况
+    findBd = re.compile(r'<p class="">(.*?)</p>', re.S)  # 找到影片相关内容
+    remove = re.compile(r'              |\n|</br>|\.*')  # 去掉无关内容
+    datalist = []
+    for i in range(0, 10):
+        # 获取数据
+        url = baseurl + str(i * 25)
+        print("url is " + url)
+        html = askURL(url)
+
+        # 解析内容
+        soup = BeautifulSoup(html, "html.parser")
+
+        for item in soup.find_all('div', class_='item'):
+            data = []  # 定义一个解析数据结果的列表
+            item = str(item)  # 转换成字符串
+
+            link = re.findall(findLink, item)[0]
+            data.append(link)  # 添加详情链接
+
+            imgSrc = re.findall(findImgSrc, item)[0]
+            data.append(imgSrc)  # 添加图片链接
+
+            titles = re.findall(findTitle, item)
+            # 片名可能只有一个中文名，没有外国名
+            if (len(titles) == 2):
+                ctitle = titles[0]
+                data.append(ctitle)  # 添加中文片名
+                otitle = titles[1].replace("/", "")  # 去掉无关符号
+                data.append(otitle)
+            else:
+                data.append(titles[0])  # 添加中文片名
+                data.append(' ')  # 留空
+            rating = re.findall(findRating, item)[0]
+            data.append(rating)  # 添加评分
+            judgeNum = re.findall(findJudge, item)[0]
+            data.append(judgeNum)  # 评论人数
+            inq = re.findall(findInq, item)
+            # print(inq)
+            # 添加概况 可能没有概况
+            if len(inq) != 0:
+                inq = inq[0].replace("。", "")  # 去掉句号
+                data.append(inq)  # 添加概况
+            else:
+                data.append(' ')
+            # print(re.findall(findBd, item))
+            bd = re.findall(findBd, item)[0]
+            # print("bd is "+bd)
+            bd = re.sub(remove, "", bd)
+            bd = re.sub('<br(\s+)?\/?>(\s+)?', " ", bd)  # 去掉<br >
+            bd = re.sub('/', " ", bd)  # 替换/
+            # data.append(bd.strip())
+            # print(bd)
+            words = bd.split('   ')
+            for s1 in words:
+                s2 = s1.split('   ')
+                for s3 in s2:
+                    if len(s3) != 0 and s3 != " ":  #
+                        # print("s3 = " + s3)
+                        # if s3.startswith("主演"):
+                            # print("s3 = " + s3)
+                            # print("s3[0:-5]=" + s3[0:-5])
+                            # print("s3[-4:]=" + s3[-4:])
+                        if (s3[-4:].isdigit()):
+                        # if s3.startswith("主演"):
+                            data.append(s3[0:-5])
+                            data.append(s3[-4:])
+                        elif s3.endswith(")"):
+                            data.append(s3[0:-11])
+                            data.append(s3[-10:])
+                        else:
+                            data.append(s3)
+            # 主演有可能因为导演内容太长而没有
+            # print(len(data))
+            if (len(data) != 12):
+                data.insert(8, ' ')  # 留空
+            datalist.append(data)
+    return datalist
+
+# 保存数据
+def saveData(datalist, savePath):
+    book = xlwt.Workbook(encoding="utf-8", style_compression=0)
+    sheet = book.add_sheet('豆瓣电影Top250', cell_overwrite_ok=True)
+    col = ('电影详情链接', '图片链接', '影片中文名', '影片外国名', '评分', '评价数', '概况', '导演', '主演', '年份', '国家', '类别')
+    for i in range(0, 12):
+        sheet.write(0, i, col[i])
+    for i in range(0, 250):
+        data = datalist[i]
+        for j in range(0, 12):
+            sheet.write(i + 1, j, data[j])
+    book.save(savePath)
+
+
+def main():
+    print("开始爬取...")
+    baseurl = "https://movie.douban.com/top250?start="
+    datalist = getData(baseurl)  # 获取数据
+    savePath = "C:\\Users\\lenovo\\Documents\\data\\豆瓣电影Top250.xls"
+    saveData(datalist, savePath)  # 保存数据
+
+
+if __name__ == '__main__':
+    main()
+    print("爬取完成，请查看.xls文件")
